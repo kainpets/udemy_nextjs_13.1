c5e1954ae896a2dbbae0dea476955ec98bba8b77
pawel_stepniak <pawelstepniak22@gmail.com>
2023-03-27 23:41:13 +0200

    add layout to restaurant details pages

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import NavBar from "../components/NavBar";
 import Header from "./components/Header";
 import SearchSidebar from "./components/SearchSidebar";
 import RestaurantCard from "./components/RestaurantCard";
@@ -8,9 +6,7 @@ export default function Search() {
 
 
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-  <main className="max-w-screen-2xl m-auto bg-white">
-    <NavBar />
+    <>
     <Header />
     <div className="flex py-4 m-auto w-2/3 justify-between items-start">
       <SearchSidebar />
@@ -18,8 +14,6 @@ export default function Search() {
       <RestaurantCard />
     </div>
     </div>
-  </main>
-</main>
-
+    </>
   )
 }
