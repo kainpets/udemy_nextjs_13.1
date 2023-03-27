5ea242eaadfeacad731d9643eba72866c14ee052
pawel_stepniak <pawelstepniak22@gmail.com>
2023-03-27 21:15:29 +0200

    refactor menu page to components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Header from "./components/Header";
+import { RestaurantNavBar } from "./components/RestaurantNavBar";
 import NavBar from "../../../components/NavBar";
 
 export default function RestaurantMenuPage() {
@@ -6,25 +7,12 @@ export default function RestaurantMenuPage() {
     <main className="bg-gray-100 min-h-screen w-screen">
   <main className="max-w-screen-2xl m-auto bg-white">
     <NavBar />
-     {/* HEADER */}
-    <div className="h-96 overflow-hidden">
-      <div
-        className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center"
-      >
-        <h1 className="text-7xl text-white captitalize text-shadow text-center">
-          Milestones Grill (Toronto)
-        </h1>
-      </div>
-    </div>
-    {/* HEADER */} {/* DESCRIPTION PORTION */}
+    <Header />
+     {/* DESCRIPTION PORTION */}
     <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
       <div className="bg-white w-[100%] rounded p-3 shadow">
-        {/* RESAURANT NAVBAR */}
-        <nav className="flex text-reg border-b pb-2">
-          <Link href="/restaurant/milestone-grill" className="mr-7"> Overview </Link>
-          <Link href="/restaurant/milestone-grill/menu" className="mr-7"> Menu </Link>
-        </nav>
-        {/* RESAURANT NAVBAR */} {/* MENU */}
+        <RestaurantNavBar />
+        {/* MENU */}
         <main className="bg-white mt-5">
           <div>
             <div className="mt-4 pb-1 mb-1">
