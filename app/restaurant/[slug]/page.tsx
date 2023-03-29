5d82b33e2769f6dd711a5605760593949efb1f84
Paweł Stępniak <pawelstepniak22@gmail.com>
2023-03-29 12:35:20 +0000

    restaurant slug not fetching the correct slug

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -6,8 +5,49 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true
+    }
+  });
+
+  // if (!restaurant) {
+  //   throw new Error("WTF");
+  // }
+
+  return restaurant;
+}
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: {slug: string};
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
+  console.log(params.slug)
+  console.log(restaurant);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
