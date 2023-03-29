152a6cce2037c1820a7ab03b6c9c0ab4f1c481b3
Paweł Stępniak <pawelstepniak22@gmail.com>
2023-03-29 13:56:29 +0000

    fetch restaurant's menu and render dynamically

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,15 +1,23 @@
+import { Item } from "@prisma/client";
 import MenuCard from "./MenuCard";
 
-export default function Menu() {
+export default function Menu({menu}: {menu: Item[]}) {
   return (
     <main className="bg-white mt-5">
           <div>
             <div className="mt-4 pb-1 mb-1">
               <h1 className="font-bold text-4xl">Menu</h1>
             </div>
+            {menu.length ? (
             <div className="flex flex-wrap justify-between">
-              <MenuCard />
-            </div>
+              {menu.map(item => (
+              <MenuCard key={item.id} item={item}/>
+              ))}
+            </div>) : (
+              <div className="flex flex-wrap justify-between">
+                <p>This restaurant does not have a menu</p>
+              </div>
+            )}
           </div>
         </main>
   )
