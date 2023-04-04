07c7a0daad5f4fb504f4a23fb0745f8716329993
Paweł Stępniak <pawelstepniak22@gmail.com>
2023-04-04 13:14:02 +0000

    fetch restaurants based on sidebar query parameters

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,29 +4,54 @@ import RestaurantCard from "./components/RestaurantCard";
 import { PRICE, PrismaClient } from "@prisma/client";
 
 
-const select = {
-  id: true,
-  name: true,
-  main_image: true,
-  price: true,
-  cuisine: true,
-  location: true,
-  slug: true
-}
 
 const prisma = new PrismaClient();
 
-const fetchRestaurantsByCity = (city: string | undefined) => {
-  if (!city) return prisma.restaurant.findMany({select});
+interface SearchParams {
+ city?: string; cuisine?: string; price?: PRICE
+}
+
+const fetchRestaurantsByCity = (searchParams: SearchParams) => {
+  const where:any = {}
+
+  if (searchParams.city) {
+    const location = {
+      name: {
+        equals: searchParams.city.toLowerCase()
+      }
+    }
+    where.location = location
+  }
+
+  if (searchParams.cuisine) {
+    const cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase()
+      }
+    }
+    where.cuisine = cuisine
+  }
+
+  if(searchParams.price) {
+    const price = {
+      equals: searchParams.price
+    }
+    where.price = price
+  }
+
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true
+  }
+
 
   return  prisma.restaurant.findMany({
-    where: {
-      location: {
-        name: {
-          equals: city.toLowerCase()
-        }
-      },
-    },
+    where,
     select,
   })
 }
@@ -40,8 +65,8 @@ const fetchCuisines = async () => {
   return prisma.cuisine.findMany();
 }
 
-export default async function Search({searchParams}: {searchParams: {city?: string, cuisine?: string, price?: PRICE}}) {
-  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+export default async function Search({searchParams}: {searchParams: SearchParams}) {
+  const restaurants = await fetchRestaurantsByCity(searchParams);
   const location = await fetchLocations();
   const cuisine = await fetchCuisines();
   
