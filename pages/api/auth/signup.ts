50576906e60c3f52aae778fb42f0c1a2fdb3e5b7
Paweł Stępniak <pawelstepniak22@gmail.com>
2023-04-25 09:08:54 +0000

    add password hashing with bcrypt

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator"
+import bcrypt from "bcrypt"
 
 const prisma = new PrismaClient();
 
@@ -61,6 +62,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (userWithEmail) {
       return res.status(400).json({errorMessage: "Email already in use"})
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
 
 
     res.status(200).json({
